9bffc9a994a9a9844b083ccf4c8e037d2f91fb34
Nick Ma <honeyday.mj@gmail.com>
2013-12-28 14:13:16 +0800

    add icon support

--- src/basic/BasicWeekView.js
+++ src/basic/BasicWeekView.js
@@ -2,52 +2,52 @@
 fcViews.basicWeek = BasicWeekView;
 
 function BasicWeekView(element, calendar) {
-	var t = this;
-	
-	
-	// exports
-	t.render = render;
-	
-	
-	// imports
-	BasicView.call(t, element, calendar, 'basicWeek');
-	var opt = t.opt;
-	var renderBasic = t.renderBasic;
-	var skipHiddenDays = t.skipHiddenDays;
-	var getCellsPerWeek = t.getCellsPerWeek;
-	var formatDates = calendar.formatDates;
-	
-	
-	function render(date, delta) {
+  var t = this;
 
-		if (delta) {
-			addDays(date, delta * 7);
-		}
 
-		var start = addDays(cloneDate(date), -((date.getDay() - opt('firstDay') + 7) % 7));
-		var end = addDays(cloneDate(start), 7);
+  // exports
+  t.render = render;
 
-		var visStart = cloneDate(start);
-		skipHiddenDays(visStart);
 
-		var visEnd = cloneDate(end);
-		skipHiddenDays(visEnd, -1, true);
+  // imports
+  BasicView.call(t, element, calendar, 'basicWeek');
+  var opt = t.opt;
+  var renderBasic = t.renderBasic;
+  var skipHiddenDays = t.skipHiddenDays;
+  var getCellsPerWeek = t.getCellsPerWeek;
+  var formatDates = calendar.formatDates;
 
-		var colCnt = getCellsPerWeek();
 
-		t.start = start;
-		t.end = end;
-		t.visStart = visStart;
-		t.visEnd = visEnd;
+  function render(date, delta) {
 
-		t.title = formatDates(
-			visStart,
-			addDays(cloneDate(visEnd), -1),
-			opt('titleFormat')
-		);
+    if (delta) {
+      addDays(date, delta * 7);
+    }
 
-		renderBasic(1, colCnt, false);
-	}
-	
-	
+    var start = addDays(cloneDate(date), -((date.getDay() - opt('firstDay') + 7) % 7));
+    var end = addDays(cloneDate(start), 7);
+
+    var visStart = cloneDate(start);
+    skipHiddenDays(visStart);
+
+    var visEnd = cloneDate(end);
+    skipHiddenDays(visEnd, -1, true);
+
+    var colCnt = getCellsPerWeek();
+
+    t.start = start;
+    t.end = end;
+    t.visStart = visStart;
+    t.visEnd = visEnd;
+
+    t.title = formatDates(
+      visStart,
+      addDays(cloneDate(visEnd), -1),
+      opt('titleFormat')
+    );
+
+    renderBasic(1, colCnt, false);
+  }
+
+
 }
